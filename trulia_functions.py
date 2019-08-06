30d9116b908ab3fd4db03db2261f2b0008b9f4e7
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-08-05 22:28:11 -0400

    Update trulia_functions.py

--- trulia_functions.py
+++ trulia_functions.py
@@ -12,13 +12,36 @@ import time
 import pandas as pd
 import numpy as np
 
+xpath {
+  'address':'tyuio',
+  'price':'ghjkl',
+  'bdr':'asf',
+  'ba':'asf',
+  'sqft':'asfd'
+}
+
+
 def go_to_trulia(browser):
   browser.get('https://www.trulia.com/')
   
-get_listings(browser):
+def get_listings(browser):
   listing_cards = WebDriverWait(browser, 10).until(
     EC.presence_of_all_elements_located((By.XPATH, xpath['listing_cards']))
   )   
     return(listing_cards)
 
-get_listing_info
+def get_listing_info(browser, info_type):
+  try:
+    attr = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, xpath[info_type]))).text
+  
+  except (NoSuchElementException, TimeoutException):
+    attr = np.nan
+    
+  return(attr)
+
+def write_data_to_file(output_data, zipcode):
+    file_name = 'Rents_'+ zipcode + '.csv'
+    columns = ["Address", "Bedrooms", "Bathrooms", "Sqft", "Price"]
+    pd.DataFrame(output_data, columns = columns).drop_duplicates().to_csv(
+    file_name, index = False, encoding = "UTF-8")
