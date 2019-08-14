286ef2945c1833c5997c7cde72c857a7f8e702f5
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-08-13 21:46:21 -0400

    Update trulia_functions.py

--- trulia_functions.py
+++ trulia_functions.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 
-import time
 import pandas as pd
+from pandas import ExcelWriter
 import numpy as np
 
 xpath {
@@ -26,10 +26,20 @@ def go_to_trulia(browser):
   browser.get('https://www.trulia.com/')
   
 def get_listings(browser):
-  listing_cards = WebDriverWait(browser, 10).until(
-    EC.presence_of_all_elements_located((By.XPATH, xpath['listing_cards']))
-  )   
-    return(listing_cards)
+  try:
+    listing_cards = WebDriverWait(browser, 10).until(
+      EC.presence_of_all_elements_located((By.XPATH, xpath['listing_cards']))
+    )
+  except (NoSuchElementException, TimeoutException):
+    try:
+      listing_cards = WebDriverWait(browser, 10).until(
+      EC.presence_of_all_elements_located((By.XPATH, '//div[@class="Grid__CellBox-sc-144isrp-0 SearchResultsList__WideCell-sc-183kqex-3 hxSIvC"]'))
+      )
+    except:   
+      print("Unable to get listing cards - abort")
+      listing_cards = np.nan
+
+  return(listing_cards)
 
 def get_listing_info(browser, info_type):
   try:
@@ -40,6 +50,44 @@ def get_listing_info(browser, info_type):
     attr = np.nan
     
   return(attr)
+
+'''Find the number of the last page of listing results to inform how many pages to loop through'''
+def get_number_of_pages(browser):
+  try:
+    pages = browser.find_elements_by_xpath('//li[@data-testid="pagination-page-link"]')
+    pages_num = int(pages[-1].text)
+
+  except:
+    try:
+      pages = browser.find_elements_by_xpath('//a[@class="pvl phm"]')
+      pages_num = int(pages[-1].text)
+
+    except Exception as e:
+      print("Unable to get number of pages - {}".format(e))
+      pages_num = 1
+
+  return(pages_num)
+
+'''Find the next page arrow for listing results and click it'''
+def go_to_next_page(browser):
+  try:
+    nextp = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//i[@class="iconRightOpen"]')))
+    nextp.click()
+  
+  except (NoSuchElementException, TimeoutException):
+    try:
+      nextp = WebDriverWait(browser, 10).until(
+              EC.visibility_of_element_located((By.XPATH, '//li[@data-testid="pagination-next-page"]')))
+      nextp.click()
+    except:
+      print("Unable to get next page - abort")
+
+def create_output_dataframe(output_data):
+  columns = ["Address", "Bedrooms", "Bathrooms", "Sqft", "Price"]
+  output_dataframe = pd.DataFrame(output_data, columns = columns).drop_duplicates()
+
+  return(output_dataframe)
 
 def write_data_to_file(output_data, zipcode):
     file_name = 'Rents_'+ zipcode + '.csv'
