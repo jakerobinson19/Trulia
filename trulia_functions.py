b8b7bd4c3abdb66defd6aa153e8a0eb26444230c
Jake Robinson <46797156+jakerobinson19@users.noreply.github.com>
2019-08-25 00:51:08 -0400

    Updated captcha check logic and cleaned up some code

--- trulia_functions.py
+++ trulia_functions.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 
+import time
+from datetime import date
 import pandas as pd
 from pandas import ExcelWriter
 import numpy as np
@@ -36,8 +37,8 @@ def go_to_trulia_url(browser, type, zipcode):
     browser.get('https://www.trulia.com/for_rent/'+ zipcode +'_zip/')
 
   elif type == 'sold':
-    browser.get('https://www.trulia.com/sold/'+zipcode+'_zip/')  
-  
+    browser.get('https://www.trulia.com/sold/'+zipcode+'_zip/')
+
 def get_current_url(browser):
     """ Get URL of the loaded webpage """
     try:
@@ -54,13 +55,13 @@ def get_current_url(browser):
   
 def get_listings(browser):
   try:
-    listing_cards = WebDriverWait(browser, 10).until(
+    listing_cards = WebDriverWait(browser, 6).until(
       EC.presence_of_all_elements_located((By.XPATH, xpath['listing_cards']))
     )
   except (NoSuchElementException, TimeoutException):
     try:
-      listing_cards = WebDriverWait(browser, 10).until(
-      EC.presence_of_all_elements_located((By.XPATH, '//div[@class="Grid__CellBox-sc-144isrp-0 SearchResultsList__WideCell-sc-183kqex-3 hxSIvC"]'))
+      listing_cards = WebDriverWait(browser, 6).until(
+      EC.presence_of_all_elements_located((By.XPATH, xpath['listing_cards2']))
       )
     except:   
       print("Unable to get listing cards - abort")
@@ -78,15 +79,14 @@ def get_listing_info(browser, info_type):
     
   return(attr)
 
-'''Find the number of the last page of listing results to inform how many pages to loop through'''
 def get_number_of_pages(browser):
   try:
-    pages = browser.find_elements_by_xpath('//li[@data-testid="pagination-page-link"]')
+    pages = browser.find_elements_by_xpath(xpath['pages'])
     pages_num = int(pages[-1].text)
 
   except:
     try:
-      pages = browser.find_elements_by_xpath('//a[@class="pvl phm"]')
+      pages = browser.find_elements_by_xpath(xpath['pages2'])
       pages_num = int(pages[-1].text)
 
     except Exception as e:
@@ -95,25 +95,21 @@ def get_number_of_pages(browser):
 
   return(pages_num)
 
-'''Find the next page arrow for listing results and click it'''
 def go_to_next_page(browser):
   try:
     nextp = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//i[@class="iconRightOpen"]')))
+            EC.visibility_of_element_located((By.XPATH, xpath['next_page'])))
     nextp.click()
   
   except (NoSuchElementException, TimeoutException):
     try:
       nextp = WebDriverWait(browser, 10).until(
-              EC.visibility_of_element_located((By.XPATH, '//li[@data-testid="pagination-next-page"]')))
+              EC.visibility_of_element_located((By.XPATH, xpath['next_page2'])))
       nextp.click()
     except:
       print("Unable to get next page - abort")
 
-'''Format data to allow for dataframe conversion and arithmetic'''
 def clean_data(data):
-  
-  #set studios to 0 bedrooms
   data.loc[data['Bedrooms'] == 'Studio', 'Bedrooms'] = 0
   data['Bedrooms'] = data.Bedrooms.astype(int)
   data['Bathrooms'] = data.Bathrooms.astype(float)
@@ -129,27 +125,27 @@ def get_price_per_sqft(price, sqft):
 
   ppersq = round(p/(int(sqft.replace(',',''))),2)
 
-  return(ppersq)     
-      
+  return(ppersq)
+
 def get_summary_data(data):
 
-  sum_data = data.groupby(['Bedrooms','Bathrooms']).agg({'Sqft':['min','max'], 'Price':['min','max','mean']})
+  sum_data = data.groupby(['Bedrooms','Bathrooms']).agg({'Sqft':['min','max','mean'], 'Price':['min','max','mean'], '$/sq':'mean'})
+  
+  return(sum_data)
 
-  return(sum_data)      
-      
 def create_output_dataframe(output_data):
   columns = ["Address", "Bedrooms", "Bathrooms", "Sqft", "Price", "$/sq"]
   output_dataframe = pd.DataFrame(output_data, columns = columns).drop_duplicates()
 
   return(output_dataframe)
 
-def check_for_captcha(browser):
+def get_captcha(browser):
   try:
-    h1 = browser.find_element_by_xpath('/html/body/section/div[2]/div/h1').text
+    h1 = browser.find_element_by_xpath(xpath['captcha']).text
     if 'verify' in h1:
       print("A wild Captcha has appeared!! Please solve it for me so I can continue :)")
       return(True)
-    
+
     else:
       return(False)
 
@@ -159,14 +155,26 @@ def check_for_captcha(browser):
         if 'captcha' in url:
           print("A wild Captcha has appeared!! Please solve it for me so I can continue :)")
           return(True)
-       
+
         else:
           return(False)
-        
+
       except:
+
         return(False)
+
+def check_for_captcha(browser):
+
+  captcha = get_captcha(browser)
+
+  if captcha:
+    while captcha:
       
-#def recursive captcha check
+      resume = input("Enter 'y' once you have completed the captcha: ")
+      
+      if resume == 'y':
+        captcha = get_captcha(browser)
+        continue
 
 def write_data_to_file(list_dfs, zipcodes):
     file_name = 'Listings_'+ str(date.today()) + '.xlsx'
